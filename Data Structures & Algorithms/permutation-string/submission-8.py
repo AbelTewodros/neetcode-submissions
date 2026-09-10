011314307c8e20c01dcc58c585dcc97320aae150
AbelTewodros <73427595+AbelTewodros@users.noreply.github.com>
2026-09-10 09:36:19 -0700

    Add: permutation-string - submission-8

---
+++ Data Structures & Algorithms/permutation-string/submission-8.py
@@ -0,0 +1,40 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        s1_counter, s2_counter = [0]*26,[0]*26
+
+        for i in range(len(s1)):
+            s1_counter[ord(s1[i])-ord('a')] += 1
+            s2_counter [ord(s2[i])-ord('a')] += 1
+        
+        
+        matches = 0
+        for k in range(len(s1_counter)):
+            if s1_counter[k] == s2_counter[k]:
+                matches += 1
+        
+        left = 0
+        for right in range(len(s1),len(s2)):
+            if matches == 26:
+                return True
+            right_idx = ord(s2[right])-ord('a')
+            s2_counter[right_idx] += 1
+            if s2_counter[right_idx] - 1 == s1_counter[right_idx]:
+                matches -= 1
+            elif s2_counter[right_idx] == s1_counter[right_idx]:
+                matches += 1
+
+            left_idx = ord(s2[left]) - ord('a')
+            s2_counter[left_idx] -= 1
+            
+            
+            if s2_counter[left_idx] + 1 == s1_counter[left_idx]:
+                matches -= 1
+            elif s2_counter[left_idx] == s1_counter[left_idx]:
+                matches += 1
+            left += 1
+        return matches == 26
+
+            
